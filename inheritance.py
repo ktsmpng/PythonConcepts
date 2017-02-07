5e098801379a65defcf9b4624fbdfbaaf5114dd8
Kitty <kitsumpang@gmail.com>
2017-02-07 00:49:12 +0000

    Learning about Inheritance

--- inheritance.py
+++ inheritance.py
@@ -4,7 +4,7 @@
 This can be applied when 'TWO OR MORE CLASSES SHARE COMMON ATTRIBUTES OR METHODS.' 
 
 Base/ Super Class - the class that is inherited from.
-Subclass - in herits from the base class.
+Subclass - inherits from the base class.
 Note: You can also have subclass of a subclass etc.. """
 
 class Pet: #Create a Base Class
@@ -16,19 +16,19 @@ class Pet: #Create a Base Class
 class Cat(Pet): #Creating a Cat class which inherits from Base Class "Pet" 
 
 	def __init__(self, name, age):
-		super().__init__(name, age) #Call SUper class in this case Pet and run its method like this"
+		super().__init__(name, age) #Calling the SUper class in this case Pet and running its method like this"
 
 def Main():
 
 		thePet = Pet("Pet", 2)
 		Garfield = Cat("Garfield", 4)
 
-		print("Is Garfield a Cat?" + str(isinstance(Garfield, Cat)))
-		print("Is Garfield a Pet?" + str(isinstance(Garfield, Pet)))
-		print("Is thePet a Cat?" + str(isinstance(thePet, Cat)))
-		print("Is thePet a Pet?" + str(isinstance(thePet, Pet)))
+		print("Is Garfield a Cat? " + str(isinstance(Garfield, Cat))) #Returns True as Garfield is an instance of Cat 
+		print("Is Garfield a Pet? " + str(isinstance(Garfield, Pet))) #Returns True as Garfield is a SUBCLASS of Pet
+		print("Is thePet a Cat? " + str(isinstance(thePet, Cat))) #Returns False as thePet DOES NOT INHERIT FROM CAT!!
+		print("Is thePet a Pet? " + str(isinstance(thePet, Pet))) #Returns True as thePet is an instance of pet
 
-		print(Garfield.name)
+		print(Garfield.name) #Returns the name of Garfield from The Cat class which is inheriting from the Super class Pet!!
 
 Main()
 
